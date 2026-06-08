6c7d304fa36c18fff8de3747086071edb4676c28
millergl89 <millergl89@gmail.com>
2026-06-08 23:28:03 +0000

    Main wrapper and update variable name

--- clean_ids.py
+++ clean_ids.py
@@ -17,3 +17,31 @@ for line in sys.stdin:
         sys.stdout.write(youtube_id + "\n")
     else:
         logging.info(f"Invalid ID: {youtube_id}")
+#!/usr/bin/env python3
+
+import sys
+import logging
+
+logging.basicConfig(
+    level=logging.INFO
+)
+
+def main():
+    valid_youtube_id_chars = (
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz"
+        "0123456789-_"
+    )
+
+    for line in sys.stdin:
+        youtube_id = line.strip()
+
+        if len(youtube_id) == 11 and all(
+            char in valid_youtube_id_chars for char in youtube_id
+        ):
+            sys.stdout.write(youtube_id + "\n")
+        else:
+            logging.info(f"Invalid ID: {youtube_id}")
+
+if __name__ == "__main__":
+    main()
